6556e734ce01fc3c79d29f99ad5a3da379a7923c
Seungwon-Choi <bestwondev@gmail.com>
2025-09-19 17:40:38 +0900

    refactor: users 테이블 email 컬럼 수정 #7

--- src/main/resources/db/changelog/V001_create_table_users.sql
+++ src/main/resources/db/changelog/V001_create_table_users.sql
@@ -5,7 +5,7 @@
 CREATE TABLE IF NOT EXISTS users
 (
     id       BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    email    VARCHAR(20)                       NOT NULL UNIQUE,
+    email    VARCHAR(255)                       NOT NULL UNIQUE,
     name     VARCHAR(20)                       NOT NULL,
     role     VARCHAR(20)                       NOT NULL,
     provider VARCHAR(30)                       NOT NULL,
